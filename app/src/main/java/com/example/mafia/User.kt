8c12a43b8f628bf59dfed095f37fbdef3c12a906
Dm1try <dima.bulyuchev@mail.ru>
2023-05-30 21:02:25 +0300

    на проверку присоединения

---
+++ app/src/main/java/com/example/mafia/User.kt
@@ -0,0 +1,3 @@
+package com.example.mafia
+
+data class User(val nickname: String)
---
+++ app/src/main/java/com/example/mafia/start/GameSettings.kt
@@ -0,0 +1,26 @@
+package com.example.mafia.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.example.mafia.databinding.ActivityGameSettingsBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class GameSettings : AppCompatActivity() {
+    private lateinit var binding: ActivityGameSettingsBinding
+    private lateinit var dataBase: DatabaseReference
+    private lateinit var edName: EditText
+    private lateinit var edSurname: EditText
+    private lateinit var edEmail: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+    }
+
+    fun onClickCloseSettings(view: View) = finish()
+}
